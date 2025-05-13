59a62cfade5d0c9458fbc7679f29c1ec5d862d49
Sumit <sumitpatil0527@gmail.com>
2025-05-13 20:27:10 +0530

    Add fetch todos feature

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
-from functions import get_todos, write_todos
+from functions import get_todos, write_todos, FILE
 
 st.title("📝 Todo App")
 st.subheader("This app will increase your productivity!")
+
+def fetch_todos():
+    todos = get_todos(FILE)
+
+    for todo in todos:
+        st.checkbox(todo)
+
+
+if __name__ == "__main__":
+    fetch_todos()
