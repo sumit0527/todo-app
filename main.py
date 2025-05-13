255194dd0361c8bc6ff78ab35e525c3a7892909e
Sumit <sumitpatil0527@gmail.com>
2025-05-13 20:40:36 +0530

    implement todo input feature and Add new todo feature

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ from functions import get_todos, write_todos, FILE
 st.title("📝 Todo App")
 st.subheader("This app will increase your productivity!")
 
+
 def fetch_todos():
     todos = get_todos(FILE)
 
     for todo in todos:
         st.checkbox(todo)
 
+def add_todo():
+    todos = get_todos()
+    todo = st.session_state['new_todo']
+    todos.append(todo + '\n')
+    write_todos(todos)
 
-if __name__ == "__main__":
-    fetch_todos()
+fetch_todos()
+
+st.text_input(label="Enter Todo:",
+              placeholder="Enter new todo here...",
+              key='new_todo', on_change=add_todo)
