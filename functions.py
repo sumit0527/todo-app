cd79777c06cfefde599add6f080d8a2aeb939c82
Sumit <sumitpatil0527@gmail.com>
2025-05-13 19:27:19 +0530

    Create a update todo method

--- functions.py
+++ functions.py
@@ -1,8 +1,21 @@
-FILE = r'TODO-APP\\todos.txt'
+FILE = r"TODO-APP\\todos.txt"
+
 
 def get_todos(FILE_PATH=FILE):
-    ''' Fetch all todos from todo file '''
-    with open(FILE, 'r') as f:
+    """Fetch all todos from todo file"""
+    with open(FILE, "r") as f:
         content = f.readlines()
     return content
 
+
+def write_todos(todos, FILE_PATH=FILE):
+    """write todos to todos.txt file"""
+    with open(FILE_PATH, "w") as f:
+        f.writelines(todos)
+
+
+x = get_todos()
+x.append("learning git")
+write_todos(x)
+
+print(x)
