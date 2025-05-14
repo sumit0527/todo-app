6393a56da36ca9283cb77c97ec4f976c487ff387
Sumit <sumitpatil0527@gmail.com>
2025-05-14 06:56:01 +0530

    modify todos file path in functions file

--- functions.py
+++ functions.py
@@ -1,17 +1,16 @@
 import os
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-FILEPATH = os.path.join(SCRIPT_DIR, "todos.txt")
+FILEPATH = r"../TODO-APP/todos.txt"
 
 
-def get_todos(FILE):
+def get_todos(FILE=FILEPATH):
     """Fetch all todos from todo file"""
     with open(FILE, "r") as f:
         content = f.readlines()
     return content
 
 
-def write_todos(todos, FILE_PATH=FILEPATH):
+def write_todos(todos, FILEPATH=FILEPATH):
     """write todos to todos.txt file"""
-    with open(FILE_PATH, "w") as f:
+    with open(FILEPATH, "w") as f:
         f.writelines(todos)
